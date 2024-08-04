3ee9536d521b0b8a09e7cf8979c3e68e02c1c32e
Gobind-Barick <gurucharanbarick15@gmail.com>
2024-08-04 10:37:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{backgroundColor:"red", color:"white" , fontSize:"100px",padding:"20px"}}>Hello moto</div>
+  )
+}
+
+export default App
