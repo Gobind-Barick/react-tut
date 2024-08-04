0125f765d1929740b5527a23bcedc585eaca7e3e
Gobind-Barick <gurucharanbarick15@gmail.com>
2024-08-04 11:19:00 +0530

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Home from './Home'
+import UsingProps from './UsingProps'
 
 
 
@@ -16,12 +17,13 @@ const App = () => {
 
   return (
     <div style={{backgroundColor:"red", color:"white" , fontSize:"100px",padding:"20px"}}>
-      <Home/>
+      <Home/> 
       
       <input style={{padding:"20px",width:"500px"}} type="number"  value={inputValue}/> 
       <button onClick={increment} style={{padding:"20px", marginLeft:"2px"}} >+</button>
       <button  onClick={decrement} style={{padding:"20px" , marginLeft:"4px"}}>-</button>
       
+      <UsingProps name={"RadheShyam"}/>
        </div>
   )
 }
---
+++ src/UsingProps.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import App from './App'
+
+const UsingProps = ({name}) => {
+  return (
+    <div style={{fontSize:"30px"}}>hello my name is {name}</div>
+  )
+}
+
+export default UsingProps
